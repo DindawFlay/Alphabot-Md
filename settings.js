eff36c811f6d5b508235124d8d189cfb9ea81739
DindawFly <97446180+DindawFlay@users.noreply.github.com>
2022-06-20 03:27:28 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,9 +13,9 @@ global.APIKeys = {
 
 // setting 
 global.autoread = true // auto read pesan / message
-global.autorecording = true //status auto merekam ( auto record )
-global.autoketik = false //status auto mengetik (auto typing)
-global.available = false //status online (online)
+global.autorecording = false //status auto merekam ( auto record )
+global.autoketik = true //status auto mengetik (auto typing)
+global.available = true //status online (online)
 
 // Other
 global.botname = "NahBotz-MD❄️ " //namabot
